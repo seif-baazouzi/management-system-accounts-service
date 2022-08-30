7d3b4236b51b54949ac3524fb49b9ce6895d04ae
seif <baazouziseif9@gmail.com>
2022-08-30 08:43:04 +0100

    init fiber

---
+++ src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Listen(":3000")
+}
