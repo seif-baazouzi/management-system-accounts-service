45d03aa25341223c775162dfeb7f464c64b5ac91
seif <baazouziseif9@gmail.com>
2022-09-01 06:55:37 +0100

    refactor tests

--- tests/routes/updateUsername.py
+++ tests/routes/updateUsername.py
@@ -7,74 +7,48 @@ def testToken():
     return res.equals({ "message": "invalid-token" })
 
 def testEmptyFields():
-    body = {
-        "username": utils.randomString(10),
-        "password": utils.randomString(10),
-    }
-    res = testRoute(POST, f"{config.server}/api/v1/signup", body=body)
-    token = res.body["token"]
-
+    username, password, token = utils.signup()    
     res = testRoute(PUT, f"{config.server}/api/v1/settings/update/username", headers={ "X-Token": token })
+    
     return res.equals({ "newUsername": "Must not be empty", "password": "Must not be empty" })
 
 def testExistingUsername():
-    user1 = {
-        "username": utils.randomString(10),
-        "password": utils.randomString(10),
-    }
-    testRoute(POST, f"{config.server}/api/v1/signup", body=user1)
+    user1Username, user1Password, _ = utils.signup()
+    username, password, token = utils.signup()
     
-    signupBody = {
-        "username": utils.randomString(10),
-        "password": utils.randomString(10),
-    }
-    res = testRoute(POST, f"{config.server}/api/v1/signup", body=signupBody)
-    token = res.body["token"]
-
     body = {
-        "newUsername": user1["username"],
-        "password": signupBody["password"],
+        "newUsername": user1Username,
+        "password": password,
     }
     res = testRoute(PUT, f"{config.server}/api/v1/settings/update/username", headers={ "X-Token": token }, body=body)
     return res.equals({ "newUsername": "This username is already exist" })
 
 def testInvalidPassword():
-    signupBody = {
-        "username": utils.randomString(10),
-        "password": utils.randomString(10),
-    }
-    res = testRoute(POST, f"{config.server}/api/v1/signup", body=signupBody)
-    token = res.body["token"]
+    username, password, token = utils.signup()
 
     body = {
         "newUsername": utils.randomString(10),
         "password": utils.randomString(10),
     }
     res = testRoute(PUT, f"{config.server}/api/v1/settings/update/username", headers={ "X-Token": token }, body=body)
+
     return res.equals({ "password": "Wrong Password" })
 
 def testUpdateUsername():
-    signupBody = {
-        "username": utils.randomString(10),
-        "password": utils.randomString(10),
-    }
-    res = testRoute(POST, f"{config.server}/api/v1/signup", body=signupBody)
-    token = res.body["token"]
+    username, password, token = utils.signup()
 
     body = {
         "newUsername": utils.randomString(10),
-        "password": signupBody["password"],
+        "password": password,
     }
     res = testRoute(PUT, f"{config.server}/api/v1/settings/update/username", headers={ "X-Token": token }, body=body)
     if not res.equals({ "message": "success" }):
         return False
 
-    loginBody = {
-        "username": body["newUsername"],
-        "password": signupBody["password"],
-    }
-    res = testRoute(POST, f"{config.server}/api/v1/login", body=loginBody)
-    return res.hasKeys("message", "token") and res.equals({ "message": "success" })
+    username = body["newUsername"]
+
+    token = utils.login(username, password)
+    return token != False
 
 tests = [
     Test("Update Username Invalid Token", testToken),
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,26 @@
 import string
 import random
 
+import config
+from testRestApi import testRoute, POST
+
 def randomString(length):
     return ''.join(random.choices(string.ascii_letters, k=length))
+
+def signup():
+    username = randomString(10)
+    password = randomString(10)
+
+    body = { "username": username, "password": password }
+    res = testRoute(POST, f"{config.server}/api/v1/signup", body=body)
+    token = False if not res.hasKeys("token") else res.body["token"]
+
+    return username, password, token
+
+def login(username, password):
+    body = { "username": username, "password": password }
+    res = testRoute(POST, f"{config.server}/api/v1/login", body=body)
+    token = False if not res.hasKeys("token") else res.body["token"]
+
+    return token
+
